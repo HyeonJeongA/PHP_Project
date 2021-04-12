e55e09aaa23a624e90de2a42d6aeeb78c5d367d6
HyeonJeongA <hja4981@gmail.com>
2021-04-12 17:59:05 +0900

    php 두번째 공부 내용입니다.

---
+++ two/string.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>문자</title>
+</head>
+<body>
+    <h1>String & String Operator</h1>
+    <?php
+        echo "Hello \"w\"ord";
+    ?>
+    
+    <h2>concatenation operator</h2>
+    <?php
+        echo "Hello "."world";
+    ?>
+
+    <h2>String length function</h2>
+    <?php
+        echo strlen("Hello world");
+    ?>
+
+    
+</body>
+</html>
