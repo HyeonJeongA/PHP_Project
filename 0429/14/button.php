fa54b1a3bb2b9a0740d161da7fb2adf8592b8cdf
HyeonJeongA <hja4981@gmail.com>
2021-05-01 19:06:02 +0900

    생활 코딩 내용입니다.

--- 0429/14/button.php
+++ 0429/14/button.php
@@ -3,7 +3,7 @@ header("Content-type : image/png"); //무조건 맨 위에 써야함! 안쓰면 
 $string = $_GET['text'];
 $im = imagecreatefrompng("button.png");
 $orange=imagecolorallocate($im, 60, 87, 156);
-$px=(imagesx($im) - 7.5 * strlen($string))/2;
+$px = (imagesx($im) - (7.5 * strlen($string))) / 2;
 imagestring($im, 4, $px, 9, $string, $orange);
 imagepng($im);
 imagedestroy($im);
